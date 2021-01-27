4765195b839c6cfbb98a40f13088fc6f11ffd81e
SOHYEON00 <ohsohyeon24@gmail.com>
2021-01-27 21:15:32 +0900

    feat: 거래내역 Transaction 컴포넌트 type에 맞게 색깔 출력 - 하나의 거래내역을 나타내는 Transaction 컴포넌트의 div에 className을 변수 colorByType으로 설정함. - transaction의 type에 따라 (수입/지출) 변수 colorByType을 바꿔주었다. - 수입일 경우 #2ecc71(초록색), 지출일 경우 tomato색(빨강색) 으로 우선 출력을 위한 스타일링 함.

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React from "react";
 
 function Transaction({ transactionObj }) {
-    const type = transactionObj.type;
-    console.log(transactionObj);
-    
-    return (
-        <div>
-            {transactionObj.date}
-            {transactionObj.text}
-            {transactionObj.amount}
-        </div>
-    )
+  const type = transactionObj.type;
+  let colorByType;
+
+  if (type === "수입") {
+    colorByType = "moneyPlus";
+  } else if (type === "지출") {
+    colorByType = "moneyMinus";
+  }
+  
+  return (
+    <div className={colorByType}>
+      <button className="deleteBtn">X</button>
+      {transactionObj.date}
+      {transactionObj.text}
+      {transactionObj.amount}
+    </div>
+  );
 }
 
-export default Transaction
+export default Transaction;
