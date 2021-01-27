dea7257888bfe2fca23cf8555abe07740560acb5
SOHYEON00 <ohsohyeon24@gmail.com>
2021-01-28 00:30:57 +0900

    temp: history 날짜별로 분류하기 위해 --- 그시 긋기
    - 상위 컴포넌트History 에서 자식 컴포넌트 Transaction으로 isLine이라는 변수를 props로 줌.
    
    - 거래객체의 날짜가 다른 경우, ---을 출력해주기 위함.
    - 기능은 구현했으나 props으로 전달해주는 게 최선인가? ** 고민 필요**

--- src/components/History.js
+++ src/components/History.js
@@ -4,20 +4,38 @@ import Transaction from "components/Transaction";
 function History({ transactions }) {
 
     const copyTransactions = transactions.concat();
-    copyTransactions.sort((a,b) => {return (a.date > b.date) ? -1 : 1})
-
+    
+    const line = "---------------";
+    copyTransactions.sort((a, b) => {
+      return a.date > b.date ? -1 : 1;
+    });
+    console.log(copyTransactions);
     return (
       <article>
         <h3>History</h3>
         <section id="historyList" className="list">
-            {copyTransactions && copyTransactions.map((transaction) => {
+            {copyTransactions && copyTransactions.map((transaction,idx) => {
+                let isLine = false;
+                if(idx < copyTransactions.length-1){
+                  if (transaction.date !== copyTransactions[idx + 1].date) {
+                    console.log(transaction.date);
+                    console.log(copyTransactions[idx + 1]);
+                    isLine = true;
+                  }
+                } else {
+                    isLine = false;
+                }
+               
                 return (
                     <Transaction
                         key={transaction.id}
                         transactionObj={transaction}
+                        isLine={isLine}
                     />
+                    
                 )
             })}
+            
         </section>
       </article>
     );
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,23 +1,28 @@
 import React from "react";
 import DeleteButton from "components/DeleteButton";
 
-function Transaction({ transactionObj }) {
+function Transaction({ transactionObj, isLine }) {
   const type = transactionObj.type;
   let colorByType = "";
+  const line = '-------';
 
   if (type === "수입") {
     colorByType = "moneyPlus";
   } else if (type === "지출") {
     colorByType = "moneyMinus";
   }
-
+console.log(isLine);
   return (
+    <>
+    
     <div className={colorByType}>
+      
       <DeleteButton transactionId={transactionObj.id} />
       {transactionObj.date}
       {transactionObj.text}
       {transactionObj.amount}
-    </div>
+    </div>{(isLine === true) && line}
+    </>
   );
 }
 
