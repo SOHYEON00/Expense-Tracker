ea7840e383a41a37ff33b320e024d9cd480bb0e6
SOHYEON00 <ohsohyeon24@gmail.com>
2021-01-29 17:27:40 +0900

    feat: 카테고리 update 기능 구현
    Category 컴포넌트
    
    doc.update() 사용해 업데이트 기능 구현

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import { CATEGORIES } from 'fBase';
+import React, {useState} from 'react';
+import { CATEGORIES, dbService } from 'fBase';
 import DeleteButton from './DeleteButton';
+// import UpdateHandler from 'components/UpdateHandler';
 
 function Category({ category }) {
+    const [newCategoryText, setNewCategoryText] = useState(category.category);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const onToggleHandler = () => {
+        setIsEditing(prev => !prev);
+    };
+
+    const onTextHandler = (e) => {
+        setNewCategoryText(e.target.value);
+    }
+    const UpdateHandler = async(e) => {
+        e.preventDefault();
+        await dbService.doc(`${CATEGORIES}/${category.id}`).update({
+            category: newCategoryText,
+        });
+        setIsEditing(false);
+    }
     return (
         <tr >
         <td>
@@ -10,8 +28,14 @@ function Category({ category }) {
         </td>
         <td>{category.type}</td>
         <td>{category.category}</td>
-        <td>
-            <button>Update</button>
+         <td>
+            <button onClick={onToggleHandler}>Update</button>
+            {isEditing && 
+                <form onSubmit={UpdateHandler}>
+                    <input type="text" value={newCategoryText} onChange={onTextHandler}/>
+                    <input type="submit"/>
+                </form>
+            }
         </td>
       </tr>
     )
